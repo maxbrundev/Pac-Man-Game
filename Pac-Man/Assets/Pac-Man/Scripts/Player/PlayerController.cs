a8a2e1b8c6014d439ddfac86132b9aa6b9026cd5
maxbrundev <maxbrundev@gmail.com>
2020-01-14 13:13:45 -0500

    Cleaning code and Updating scene

--- Pac-Man/Assets/Pac-Man/Scripts/Player/PlayerController.cs
+++ Pac-Man/Assets/Pac-Man/Scripts/Player/PlayerController.cs
@@ -37,25 +37,11 @@ namespace PacMan
         // Update is called once per frame
         void Update()
         {
-            float horizontalDirection = m_playerInputs.inputRaw.x;
-            float verticalDirection = m_playerInputs.inputRaw.y;
-
-            if (horizontalDirection > 0.5f || horizontalDirection < -0.5f)
-            {
-                m_velocityDirection = Vector2.right * horizontalDirection;
-            }
-            else if (verticalDirection > 0.5f || verticalDirection < -0.5f)
-            {
-                m_velocityDirection = Vector2.up * verticalDirection;
-            }
-
-            m_velocityDirection.Normalize();
+            CalculateVelocityDirection();
 
             m_velocityDirection *= m_speed;
 
-            Vector2 smoothedVelocity = Vector2.SmoothDamp(m_rigidbody.velocity, m_velocityDirection, ref m_velocityRef, m_smoothTime);
-
-            m_rigidbody.velocity = smoothedVelocity;
+            Move(m_velocityDirection);
 
             //m_rigidbody.velocity = direction * Time.deltaTime;
         }
@@ -63,6 +49,30 @@ namespace PacMan
         void FixedUpdate()
         {
 
+        }
+
+        private void CalculateVelocityDirection()
+        {
+            float horizontalInput = m_playerInputs.inputRaw.x;
+            float verticalInput = m_playerInputs.inputRaw.y;
+
+            if (horizontalInput > 0.5f || horizontalInput < -0.5f)
+            {
+                m_velocityDirection = Vector2.right * horizontalInput;
+            }
+            else if (verticalInput > 0.5f || verticalInput < -0.5f)
+            {
+                m_velocityDirection = Vector2.up * verticalInput;
+            }
+
+            m_velocityDirection.Normalize();
+        }
+
+        private void Move(Vector2 p_targetDirection)
+        {
+            Vector2 smoothedVelocity = Vector2.SmoothDamp(m_rigidbody.velocity, p_targetDirection, ref m_velocityRef, m_smoothTime);
+
+            m_rigidbody.velocity = smoothedVelocity;
         }
 
         private void GetComponents()
