831ec8cdb7d090e9b043f1382add3693fbf70a39
maxbrundev <maxbrundev@gmail.com>
2020-01-14 11:21:23 -0500

    Adding PlayerController script

---
+++ Pac-Man/Assets/Pac-Man/Scripts/Player/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D m_rigidbody;
+
+    private Vector2 m_direction = Vector2.zero;
+
+    [Header("MOVEMENTS PARAMETERS")]
+    [SerializeField] private float m_speed;
+
+    void Awake()
+    {
+        GetComponents();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void GetComponents()
+    {
+        m_rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void InitComponents()
+    {
+
+    }
+}
