883c7ac00c72a10c9b64aa58f8150775fe45abc6
maxbrundev <maxbrundev@gmail.com>
2020-01-14 11:57:43 -0500

    Adding namespace and PlayerInputs class

--- Pac-Man/Assets/Pac-Man/Scripts/Player/PlayerController.cs
+++ Pac-Man/Assets/Pac-Man/Scripts/Player/PlayerController.cs
@@ -2,40 +2,64 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
-public class PlayerController : MonoBehaviour
+namespace PacMan
 {
-    private Rigidbody2D m_rigidbody;
-
-    private Vector2 m_direction = Vector2.zero;
-
-    [Header("MOVEMENTS PARAMETERS")]
-    [SerializeField] private float m_speed;
-
-    void Awake()
+    [RequireComponent(typeof(Rigidbody2D))]
+    [RequireComponent(typeof(Collider2D))]
+    public class PlayerController : MonoBehaviour
     {
-        GetComponents();
-    }
+        private PlayerInputs m_playerInputs;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+        private Transform m_transform;
+        private Rigidbody2D m_rigidbody;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+        private Vector2 m_initialPosition;
+        private Vector2 m_direction = Vector2.zero;
 
-    private void GetComponents()
-    {
-        m_rigidbody = GetComponent<Rigidbody2D>();
-    }
+        [Header("MOVEMENTS PARAMETERS")]
+        [SerializeField] private float m_speed;
 
-    private void InitComponents()
-    {
+        void Awake()
+        {
+            GetComponents();
+        }
 
+        // Start is called before the first frame update
+        void Start()
+        {
+            InitComponents();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void FixedUpdate()
+        {
+
+        }
+
+        private void GetComponents()
+        {
+            m_playerInputs = GetComponent<PlayerInputs>();
+            m_transform = GetComponent<Transform>();
+            m_rigidbody = GetComponent<Rigidbody2D>();
+        }
+
+        private void InitComponents()
+        {
+            if (m_rigidbody != null)
+            {
+                m_rigidbody.gravityScale = 0.0f;
+                m_rigidbody.constraints = RigidbodyConstraints2D.FreezeRotation;
+            }
+
+            if (m_transform != null)
+            {
+                m_initialPosition = transform.localPosition;
+            }
+        }
     }
 }
---
+++ Pac-Man/Assets/Pac-Man/Scripts/Player/PlayerInputs.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PacMan
+{
+    public class PlayerInputs : MonoBehaviour
+    {
+        [Header("INPUTS BINDING")]
+        [SerializeField] private string m_horizontalAxisInput;
+        [SerializeField] private string m_verticalAxisInput;
+
+        public Vector2 input
+        {
+            get
+            {
+                Vector2 input = Vector2.zero;
+                input.x = Input.GetAxis(m_horizontalAxisInput);
+                input.y = Input.GetAxis(m_verticalAxisInput);
+                return input;
+            }
+        }
+
+        public Vector2 inputRaw
+        {
+            get
+            {
+                Vector2 input = Vector2.zero;
+                input.x = Input.GetAxisRaw(m_horizontalAxisInput);
+                input.y = Input.GetAxisRaw(m_verticalAxisInput);
+                return input;
+            }
+        }
+    }
+}
