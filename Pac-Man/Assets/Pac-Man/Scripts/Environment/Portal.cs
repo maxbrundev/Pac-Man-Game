ba45d26022591a79935a9a54d48932da1bb94ee5
maxbrundev <maxbrundev@gmail.com>
2020-01-14 15:31:15 -0500

    Adding portal for out-screen player displacement

---
+++ Pac-Man/Assets/Pac-Man/Scripts/Environment/Portal.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    [SerializeField] private Transform m_exitPosition;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Teleport(other.gameObject);
+        }
+    }
+
+    public void Teleport(GameObject p_object)
+    {
+        p_object.transform.localPosition = m_exitPosition.position;
+    }
+}
